0ad2483ab4472ea0de9de16a9651b540bbe11e42
MrPowers <matthewkevinpowers@gmail.com>
2021-03-10 12:55:52 -0300

    Demonstrate saveAsTable works with empty DataFrame

--- src/test/scala/mrpowers/delta/examples/CreateATableSpec.scala
+++ src/test/scala/mrpowers/delta/examples/CreateATableSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.FunSpec
 import com.github.mrpowers.spark.daria.sql.SparkSessionExt._
 import com.github.mrpowers.spark.daria.utils.NioUtils
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import org.apache.spark.sql.{Row, SaveMode}
 import org.apache.spark.sql.types._
 
 class CreateATableSpec extends FunSpec with SparkSessionTestWrapper with DataFrameComparer {
@@ -42,6 +43,16 @@ class CreateATableSpec extends FunSpec with SparkSessionTestWrapper with DataFra
     val df = spark.range(3)
     df.write.format("delta").saveAsTable("whatever")
     if (os.exists(whatever)) os.remove.all(whatever)
+  }
+
+  it("creates an empty table with saveAsTable") {
+    val b = os.pwd/"spark-warehouse"/"b"
+    if (os.exists(b)) os.remove.all(b)
+    val df = spark.createDataFrame(
+      spark.sparkContext.emptyRDD[Row],
+      new StructType().add("name", StringType))
+    df.write.format("delta").mode(SaveMode.Overwrite).saveAsTable("b")
+    if (os.exists(b)) os.remove.all(b)
   }
 
 }
